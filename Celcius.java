7e9f72185f9b4cb973aa1375d5cf08e17e8fe840
CemilDumanBilgeAdamBoost <90190180+CemilDumanBilgeAdamBoost@users.noreply.github.com>
2021-09-14 22:09:44 +0300

    Celcius to Fahrenheit Converter

---
+++ Celcius.java
@@ -0,0 +1,17 @@
+package com.BilgeAdam.cemil;
+
+public class Celcius {
+
+public static void main(String[] args) {
+
+double Celcius; double Fahrenheit; double Ara;
+
+Fahrenheit = 80;
+
+Ara = Fahrenheit-32;
+
+Celcius = Ara*5/9;
+
+System.out.println(Celcius);
+}
+}
